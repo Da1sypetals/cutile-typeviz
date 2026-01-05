bb1e08d07b250d6d4eb76abf0ab005c4e5665978
Da1sypetals <da1sypetals.iota@gmail.com>
2026-01-05 11:38:09 +0800

    Implement a seemingly correct sinkhorn backward (could be wrong actually)

--- examples/sinkhorn.cutile.py
+++ examples/sinkhorn.cutile.py
@@ -37,17 +37,36 @@ def sinkhorn_knopp(mat, out, num_iter, tilesize: ct.Constant[int]):
     )
 
 
+tilesize = 32
+
+
+@ct.function(host=False, tile=True)
+def matvec_A(R, x):  # x: (tilesize, n_stream*2, 1)
+    x1 = ct.extract(x, index=(0, 0, 0), shape=(tilesize, n_stream, 1))
+    x2 = ct.extract(x, index=(0, 1, 0), shape=(tilesize, n_stream, 1))
+    ax1 = x1 + ct.matmul(R, x2)
+    ax2 = ct.matmul(R.transpose(-2, -1), x1) + x2
+    return ct.cat((ax1, ax2), axis=-2)  # (tilesize, n_stream*2, 1)
+
+
+@ct.function(host=False, tile=True)
+def dot(a, b):  # a/b: (..., dim, 1)
+    return ct.matmul(a.transpose(-2, -1), b)
+
+
 @ct.kernel
-def sinkhorn_knopp_bwd_implicit(out, dout, res, num_iter, tilesize: ct.Constant[int]):
+def sinkhorn_knopp_bwd_implicit(out, dout, res, num_iter):
     """
     <typecheck>
     MockTensor((batch, seq_len, n_stream, n_stream), dtype="float32")
     MockTensor((batch, seq_len, n_stream, n_stream), dtype="float32")
     MockTensor((batch, seq_len, n_stream, n_stream), dtype="float32")
     20
-    32
     </typecheck>
+
+    Typically, num_iter = n_stream * 2
     """
+
     i_batch = ct.bid(0)
     i_seq = ct.bid(1)
 
@@ -66,23 +85,36 @@ def sinkhorn_knopp_bwd_implicit(out, dout, res, num_iter, tilesize: ct.Constant[
     dR = dR.reshape((tilesize, n_stream, n_stream))
 
     diffR = R - dR
-    r = ct.sum(diffR, axis=-1).reshape((tilesize, n_stream, 1))
-    c = ct.sum(diffR, axis=-2).reshape((tilesize, n_stream, 1))
+    # row sum
+    b1 = ct.sum(diffR, axis=-1).reshape((tilesize, n_stream, 1))
+    # col sum
+    b2 = ct.sum(diffR, axis=-2).reshape((tilesize, n_stream, 1))
 
-    # TODO: sol = solve_linear(A, b)
-    alpha = ct.zeros((tilesize, n_stream, 1), dtype=ct.float32)
-    beta = ct.zeros((tilesize, n_stream, 1), dtype=ct.float32)
+    b = ct.cat((b1, b2), axis=-2)
 
+    # Solve: Ax=b
     R = R.reshape((tilesize, n_stream, n_stream))
+    # Conjugate Gradients: init
+    x = ct.zeros((tilesize, n_stream * 2, 1), dtype=ct.float32)
+    r = b - matvec_A(R, x)
+    p = r
+    r_normsq = dot(r, r)
 
+    # Conjugate Gradients: iter
     for _ in range(num_iter):
-        alpha = r - ct.matmul(R, beta)
-        beta = c - ct.matmul(R.transpose(-2, -1), alpha)
+        Ap = matvec_A(R, p)
+        alpha = r_normsq / dot(p, Ap)
+        x += alpha * p
+        r -= alpha * Ap
+        r_new_normsq = dot(r, r)
+        beta = r_new_normsq / r_normsq
+        p = r + beta * p
+        r_normsq = r_new_normsq
 
-    alpha = alpha.reshape((tilesize, n_stream))
-    beta = beta.reshape((tilesize, n_stream))
+    x1 = ct.extract(x, index=(0, 0, 0), shape=(tilesize, n_stream, 1)).reshape((tilesize, n_stream))
+    x2 = ct.extract(x, index=(0, 1, 0), shape=(tilesize, n_stream, 1)).reshape((tilesize, n_stream))
 
-    res_tile = dR - ct.expand_dims(alpha, -1) - ct.expand_dims(beta, -2)
+    res_tile = dR - ct.expand_dims(x1, -1) - ct.expand_dims(x2, -2)
 
     ct.store(
         res,
